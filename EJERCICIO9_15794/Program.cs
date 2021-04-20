ba3bb1cee6f0905d3552f019e8e6150ae1dbbc58
EmiLuisDo <emiluisdo@gmail.com>
2021-04-20 11:06:42 -0300

    Completado el ejercicio 9

---
+++ EJERCICIO9_15794/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EJERCICIO9_15794
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            Task t = Program.ejecutarMetodo1Async();
+            Program.ejecutarMetodo2();
+
+            await t;
+            Console.ReadKey();
+        }
+
+        static async Task ejecutarMetodo1Async()
+        {
+            for(int i=0; i<100; i++)
+            {
+                await Task.Run(() => 
+                    imprimirMensaje()
+                );
+            }
+        }
+
+        private async static Task<int> imprimirMensaje()
+        {
+            await Task.Delay(100);
+            Console.WriteLine("Método1");
+            return 1;
+        }
+
+        static void ejecutarMetodo2()
+        {
+            for(int i=0; i<25; i++)
+            {
+                Task.Delay(100).Wait();
+                Console.WriteLine("Método2");
+            }
+        }
+    }
+}
