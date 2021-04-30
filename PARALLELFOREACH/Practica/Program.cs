047603dd7cd22775c5fe4b4dcfd2ccf7e23f16f1
EmiLuisDo <emiluisdo@gmail.com>
2021-04-30 11:54:30 -0300

    Revisado el papallel.for, sigue sin funcar

--- PARALLELFOREACH/Practica/Program.cs
+++ PARALLELFOREACH/Practica/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Practica
@@ -8,21 +9,35 @@ namespace Practica
     {
         static void Main(string[] args)
         {
-            /*int cantColumnasA = 1500, cantFilasB = 1500;   //estas dos cantidades tienen que coincidir
-            int cantFilasA = 1000;
-            int cantColumnasB = 1000;
+            int cantColumnasA = 500, cantFilasB = 500;   //estas dos cantidades tienen que coincidir
+            int cantFilasA = 500;
+            int cantColumnasB = 500;
             //con estas lineas definimos el tamaño de las matrices a tratar, se pueden variar para probar el rendimiento del programa
 
             double [, ] mA = inicializarMatriz(cantFilasA, cantColumnasA);
-            double [, ] mB = inicializarMatriz(cantFilasB, cantColumnasB);*/
+            double [, ] mB = inicializarMatriz(cantFilasB, cantColumnasB);
 
-            int cantColumnasA = 2, cantFilasB = 2;   //estas dos cantidades tienen que coincidir
-            int cantFilasA = 3;
+            /*int cantColumnasA = 4, cantFilasB = 4;   //estas dos cantidades tienen que coincidir
+            int cantFilasA = 5;
             int cantColumnasB = 4;
-            double [, ] mA = {{2, 3}, {4, 5},{5, 6}}; 
-            double [, ] mB = {{5, 7, 3, 7}, {4, 2, 1, 2}}; 
+            double [, ] mA ={
+                                {2, 3, 4,   6}, 
+                                {4, 5, 93, 44},
+                                {5, 6, 234, 43},
+                                {10,80, 43, 444},
+                                {34,56, 434, 44}
+                            }; 
+            double [, ] mB ={
+                                {5, 7, 3, 7}, 
+                                {4, 2, 1, 2}, 
+                                {32, 322, 443, 222},
+                                {12, 234, 21, 32222}, 
+                                {1212, 435, 23, 543},
+                                {222, 232, 122, 3333}, 
+                            }; 
+            
             imprimirMatriz(cantColumnasA, cantFilasA, mA);
-            imprimirMatriz(cantColumnasB, cantFilasB, mB);
+            imprimirMatriz(cantColumnasB, cantFilasB, mB);*/
          //codigo para testear el funcionamiento del producto, crea dos matrices pequeñas de manera estatica si se usa comentar las dos líneas inicializarMatriz()
 
 
@@ -32,8 +47,7 @@ namespace Practica
             //ejecutar multiplicacion secuencial 
             Double [,] resultS = muliplicarMatrizSecuencial(mA, mB);
             stopwatch.Stop();
-            Console.Error.WriteLine("Tiempo for secuencial (segundos): {0}", stopwatch.ElapsedMilliseconds/1000);
-
+            var tiempoSecuencial =  stopwatch.ElapsedMilliseconds/1000;
 
             stopwatch.Reset();
             stopwatch.Start();
@@ -41,9 +55,10 @@ namespace Practica
             Double [,] resultP = muliplicarMatrizParalelo(mA, mB);
             
             stopwatch.Stop();
+            Console.Error.WriteLine("Tiempo for secuencial (segundos): {0}", tiempoSecuencial);
             Console.Error.WriteLine("Tiempo for Paralelo (segundos): {0}", stopwatch.ElapsedMilliseconds/1000);
 
-            imprimirMatriz( cantColumnasB, cantFilasA, resultP);
+            //imprimirMatriz( cantColumnasB, cantFilasA, resultS);
 
         }
         static double [,] muliplicarMatrizParalelo(double [, ] mA, double[, ] mB)
@@ -54,21 +69,27 @@ namespace Practica
             int filasB = mB.GetLength(0);
             int columnasB = mB.GetLength(1);
 
-            double temp;
             double[, ] resultado = new double[ filasA, columnasB];
             Parallel.For(0, filasA, i=>
                 {
+                    //Console.WriteLine("thread" + Thread.CurrentThread.ManagedThreadId);
                     for(int j = 0; j < columnasB; j++)
-                    {   
-                        temp = 0;
-                        for(int z=0; z < columnasA ; z++)
-                        {
-                            temp += (mA[i, z] * mB[z, j]);
-                        }
-                        resultado[i,j] = temp;
+                    {
+                        resultado[i,j] = calcularElementoMatrizResultado(i, j, mA, mB, columnasA, columnasB);
                     }
                 });
             return resultado;
+        }
+
+        static double calcularElementoMatrizResultado(int i, int j, double[,] mA, double[,] mB, int columnasA, int columnasB)
+        {
+            double temp = 0;
+            for(int z=0; z < columnasA ; z++)
+            {
+                temp += (mA[i, z] * mB[z, j]);
+            }
+            return temp;
+            
         }
 
         static double [,] muliplicarMatrizSecuencial(double [, ] mA, double[, ] mB)
@@ -83,18 +104,12 @@ namespace Practica
             double[, ] resultado = new double[ filasA, columnasB];
             //Console.WriteLine("Filas result: "+resultado.GetLength(0).ToString()+", Columnas result: "+ resultado.GetLength(1).ToString());
 
-
-            double temp;
             for (int i = 0; i < filasA; i++)
             {
+                //Console.WriteLine("thread" + Thread.CurrentThread.ManagedThreadId);
                 for(int j = 0; j < columnasB; j++)
                 {
-                    temp = 0;
-                    for(int z=0; z < columnasA ; z++)
-                    {
-                        temp += (mA[i, z] * mB[z, j]);
-                    }
-                    resultado[i,j] = temp;
+                    resultado[i,j] = calcularElementoMatrizResultado(i, j, mA, mB, columnasA, columnasB);
                 }
             }
             return resultado;
